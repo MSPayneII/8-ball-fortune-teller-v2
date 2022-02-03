6cbfc885edcdf7912a647ccb0da869d37a680260
Michael Payne <mpaynei@umich.edu>
2022-02-03 10:33:55 -0500

    updating files

--- server/models/EBAnswers.js
+++ server/models/EBAnswers.js
@@ -15,13 +15,6 @@ const EBAnswersSchema = new mongoose.Schema({
 
 let EBAnswers = mongoose.model("EBAnswer", EBAnswersSchema);
 
-// if (EBAnswers) {
-//   console.log("exists");
-// } else {
 // const answer = EBAnswers.insertMany(ebData);
 
-// // EBAnswers.insertMany(data, function (error, docs) {});
-
-// console.log(answer);
-// answer.save().then(() => console.log("answer created"));
 export default EBAnswers;
